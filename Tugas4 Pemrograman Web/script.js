25a3f3b0e434da1edd42ffbee8510a40e7683158
pahwanaa <72696388+pahwanaa@users.noreply.github.com>
2020-11-14 03:55:53 +0700

    Upload Temlate
    upload aja
    upload

---
+++ Tugas4 Pemrograman Web/script.js
@@ -0,0 +1,29 @@
+alert('Selamat Datang di Kalkulator Saya')
+
+function insert(num)
+{
+    document.form.hasil.value = document.form.hasil.value+num;
+}
+
+function equal()
+{
+    const exp = document. form.hasil.value;
+    if(exp)
+    {
+        document.form.hasil.value = eval(exp);
+        alert("Hasilnya Adalah: "+eval(exp));
+    }
+    
+}
+
+
+function clean()
+{
+    document.form.hasil.value = "";
+}
+
+function back()
+{
+    const exp = document.form.hasil.value;
+    document.form.hasil.value = exp.substring(0,exp.length-1);
+}
